af017e56140c61eaec4dfcafff1b61dfcaba56fe
sdhar <sanjibdhar222@gmail.com>
2011-11-16 19:44:05 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kolkatab2b::Application.config.secret_token = '2a6a8f06ed1f37151d083e434351eec2f2068f90d7854620df895f55a73fd1c7e28a2171a526220c0a38237e17b50024f39203c11a0584d2f0c22290f0a90732'
